18f35c7317460814e1070f6cdbd48df6fe0abe95
felixreverett <felixreverett@gmail.com>
2023-06-12 16:56:25 +0100

    Changelog 12/06/2023b "Added startup framework"
    Additions:
    added to Main() method to initialise new game

--- Program.cs
+++ Program.cs
@@ -5,16 +5,53 @@ namespace TextBasedAdventureGame
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) //Todo: add a difficulty level for new games
         {
+            Console.Write(
+                "╔═══════════════════════════════════════════════╗\n" +
+                "║ Welcome to Felix's Text-Based Adventure Game! ║\n" +
+                "╚═══════════════════════════════════════════════╝\n" +
+                "> Please select between LOAD GAME and NEW GAME.\n> ");
             
-            Game game = new Game("default username");
+            bool isSelecting = true; string input = "none";
+
+            while (isSelecting)
+            {
+                input = Console.ReadLine()!.ToLower().Split(" ")[0];
+                if (input is "new" or "load")
+                {
+                    isSelecting = false;
+                }
+                else
+                {
+                    Console.Write("> Please write a valid input.\n> ");
+                }
+            }
+
+            if (input == "new")
+            {
+                Console.WriteLine(
+                    "╔═══════════════════════════════════════════════╗\n" +
+                    "║            Starting a NEW GAME!               ║\n" +
+                    "╚═══════════════════════════════════════════════╝");
+                
+                string username = string.Empty;
+
+                while (username == string.Empty)
+                {
+                    Console.Write("> Please choose a username: ");
+                    username = Console.ReadLine()!;
+                }
+
+                Game game = new Game(username);
+                game.GameStartup();
+            }
+
+            else
+            {
+                Console.WriteLine("Sorry, this isn't added yet");
+            }
             
-            game.GameStartup();
-
-            //string jsonString = JsonSerializer.Serialize(mySeason);
-            //File.WriteAllText(@$"D:\Programming\C#\TextBasedAdventureGame\Resources\Seasons\{mySeason.seasonName}.json", jsonString);
-
         }
     }
 }
